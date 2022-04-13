4d1103caa6b1c68e573cba84e9d0c1b557edcf37
kurosaki <kurosaki7492@gmail.com>
2022-04-13 13:20:39 +0800

    Add project files.

---
+++ AbilityScoreCalculator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace AbilityScoreCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoreCalculator m_scoreCalculator = new ScoreCalculator();
+
+            while(true)
+            {
+
+                m_scoreCalculator.m_rollResult = 
+                    ReadInt(m_scoreCalculator.m_rollResult, "六面骰子擲4次並加總");
+
+                m_scoreCalculator.m_divideBy = 
+                    ReadDouble(m_scoreCalculator.m_divideBy, "將結果除以");
+                
+                m_scoreCalculator.m_addAmount = 
+                    ReadInt(m_scoreCalculator.m_addAmount, "再加上");
+          
+                m_scoreCalculator.m_minimum = 
+                    ReadInt(m_scoreCalculator.m_minimum, "最小值");
+
+                m_scoreCalculator.CalculatorAbilityScore();
+
+                Console.WriteLine("能力值: " + m_scoreCalculator.m_score);
+                Console.WriteLine("按Q離開，或按其他鍵繼續。");
+
+                char m_keyChar = Console.ReadKey(true).KeyChar;
+
+                if ((m_keyChar == 'Q') || (m_keyChar == 'q')) return;
+
+            }
+        }
+
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " ["+ lastUsedValue + " ]:");
+            string m_customInput = Console.ReadLine();
+            if (double.TryParse(m_customInput, out double value))
+            {
+                Console.WriteLine("    使用輸入數值" + value);
+                return value;
+            }
+
+            else
+            {
+                Console.WriteLine("    使用預設值" + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+        /// <summary>
+        /// 寫入一個提示，並且從主控台讀取一個int值
+        /// </summary>
+        /// <param name="lastUsedValue">預設值</param>
+        /// <param name="prompt">要印到主控台的提示</param>
+        /// <returns>讀取的int值，或者當無法轉換時，使用預設值</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string m_customInput = Console.ReadLine();
+            if(int.TryParse(m_customInput, out int value))
+            {
+                Console.WriteLine("    使用輸入數值" + value);
+                return value;
+            }
+
+            else
+            {
+                Console.WriteLine("    使用預設值" + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
---
+++ AbilityScoreCalculator/ScoreCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbilityScoreCalculator
+{
+    class ScoreCalculator
+    {
+        public int m_rollResult = 14;
+        public double m_divideBy = 1.75;
+        public int m_addAmount = 2;
+        public int m_minimum = 3;
+        public int m_score;
+
+        public void CalculatorAbilityScore()
+        {
+            double m_divided = m_rollResult / m_divideBy;
+            /*int m_added = ( m_addAmount = m_addAmount + (int)m_divided);
+              也可以寫成: int m_added = m_addAmount += (int)m_divided;
+              m_addAmount 也會被賦值為: m_addAmount + (int)m_divided
+             這不是我們要的結果，所以要把等號刪掉 */
+            int m_added = m_addAmount + (int)m_divided;
+
+            if(m_added < m_minimum)
+            {
+                m_score = m_minimum;
+            }
+
+            else
+            {
+                m_score = m_added;
+            }
+        }
+
+    }
+}
